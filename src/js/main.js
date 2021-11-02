03c54b6eae9b19a1abf72afe5b146d44aa35b549
Async <sunsei288@gmail.com>
2021-11-02 03:03:04 +0300

    finish html and css

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,34 @@
 "use strict";
+import { openModal, closeModal } from './modules/popups'
+window.addEventListener("DOMContentLoaded", function () {
+  const page = document.querySelector(".page");
+  const header = page.querySelector(".header");
+  const hero = document.querySelector(".hero");
+
+  const heroHeight = hero.scrollHeight;
+  let menuStatus = false;
+  window.addEventListener("scroll", headerFixed);
+  function headerFixed() {
+    if (window.pageYOffset >= heroHeight && menuStatus === false) {
+      header.classList.add("header_fixed");
+      menuStatus = true;
+    } else if (window.pageYOffset < 100 && menuStatus === true) {
+      menuStatus = false;
+      header.classList.remove("header_fixed");
+    }
+  }
+  const btns = document.querySelectorAll('.btn');
+  btns.forEach(btn => {
+    if(btn.getAttribute('data-modal') == '' )
+    btn.addEventListener('click', () => {
+      openModal('.popup');
+    })
+  })
+  const popup = document.querySelector('.popup');
+  popup.addEventListener('click', (e) => {
+    if (e.target === popup || e.target.getAttribute("data-close") == "") {
+      closeModal(".popup");
+    }
+  })
+
+});
--- src/js/modules/popups.js
+++ src/js/modules/popups.js
@@ -1,39 +1,16 @@
-import Form from "./forms";
-const popups = function () {
-  const popupBtns = document.querySelectorAll(".btn");
-  const popup = document.querySelector(".popup");
-  const closeBtn = popup.querySelector(".popup__close");
-  popupBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      if (btn.classList.contains("btn_promo") ) {
-        popup.classList.add("active");
-        document.body.classList.add("active");
-      } else if (btn.classList.contains("btn_price")) {
-        document.body.classList.add("active");
-        popup.classList.add("active");
-        const form = popup.querySelector('form');
-        const formTitle = form.querySelector('.form__title');
-        const formBtn = form.querySelector('.btn');
-        formTitle.textContent = 'Заполните эту форму прямо сейчас!';
-        formBtn.textContent = 'Узнать подробнее';
-      }
-    });
-  });
-  popup.addEventListener("click", (e) => {
-    if (e.target === popup) {
-      closePopup ()
-    }
-  });
-  closeBtn.addEventListener("click", () => {
-   
-      closePopup ()
-    
-  });
-  function closePopup () {
-    popup.classList.remove("active");
-    document.body.classList.remove("active");
-    const form = new Form("form").clearInputs();
-  }
-};
+function closeModal(modalSelector) {
+  const modal = document.querySelector(modalSelector);
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+  document.body.classList.remove("active");
+}
 
-export default popups;
+function openModal(modalSelector) {
+  const modal = document.querySelector(modalSelector);
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+  document.body.classList.add("active");
+}
+
+export { closeModal };
+export { openModal };
